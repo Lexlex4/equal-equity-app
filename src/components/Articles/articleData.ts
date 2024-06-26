b111a62abdad491a19b6067c6b58092a4fbd29d1
LexLex4 <alexiscooperdervan@hotmail.co.uk>
2024-06-26 15:26:59 +0100

    article data

--- src/components/Articles/articleData.ts
+++ src/components/Articles/articleData.ts
@@ -51,14 +51,78 @@ export const articlesData = [
   },
   {
     id: 2,
-    title: 'Article Title 2',
-    summary: 'Summary of Article 2',
-    content: 'Full content of Article 2',
+    title: 'Sri Lanka reaches deal with creditor nations over $5.8bn of debt',
+    summary: 'Agreement to restructure borrowings is ‘significant milestone’ towards ending debt crisis, says finance minister',
+    content: `
+        Sri Lanka has completed a restructuring of $5.8bn of debt owed to a group of creditor nations, marking a significant step in the cash-strapped south Asian nation’s efforts to exit a two-year-old default.
+        
+        The finalisation of a deal with a committee of official lenders including India, Japan and France in Paris on Wednesday was a “significant milestone” in ending Sri Lanka’s debt crisis, the country’s finance minister Shehan Semasinghe said on Wednesday. The country was also in the process of signing a deal with China’s Export-Import Bank to restructure more than $4bn of debt, he added.
+        
+        Sri Lanka has been struggling to overhaul its debts owed to both public and private sector creditors since a sharp economic downturn and currency shortages pushed it to default in 2022. President Ranil Wickremesinghe’s government is also racing to restructure a further $13bn of external debt owed to bondholders, as he tries to guide the country out of an acute economic crisis ahead of elections later in the year.
+        
+        The country has been an important but volatile test case of how China will co-operate with other lenders to resolve defaults after its Belt and Road Initiative lending surge over the past decade made it the largest creditor to developing economies.
+        
+        Early debt relief negotiations were beset by delays as Beijing eschewed the standard international framework for dealing with debt crises, and insisted on differing treatment for debts owed to a range of Chinese banks.
+        
+        Sri Lanka also needs to finalise a separate deal with China Development Bank, which is classed as a commercial creditor that is owed about $2.1bn.
+        
+        Details of the deal announced on Wednesday have not been disclosed. Full terms of the relief from China — which was not part of that deal — or other official creditors are yet to be disclosed.
+        
+        “The Export-Import Bank of China was the first to tentatively agree with Sri Lanka on debt treatment [in early 2023], which has been crucial for Sri Lanka to advance fiscal reform and win further support from international creditors,” China’s foreign ministry said on Wednesday.
+        
+        Earlier this month, the IMF’s executive board called for the “comparable, transparent and timely completion” of talks with private creditors as it authorised the latest $336mn tranche of Sri Lanka’s bailout.
+        
+        In return for accepting losses of roughly one-third, Sri Lanka’s international bondholders have proposed turning their debt into so-called “macro-linked” bonds, which would offer larger payouts if Sri Lanka’s economy outperforms IMF targets. A first round of talks ended in recent weeks with differences over how to define the GDP trigger for these bonds. Sri Lanka is due to resume the talks soon.
+    `,
   },
   {
     id: 3,
-    title: 'Article Title 3',
-    summary: 'Summary of Article 3',
-    content: 'Full content of Article 3',
+    title: 'Russia begins trial of Evan Gershkovich on espionage charges',
+    summary: 'Secret proceedings begin against Wall Street Journal reporter as US continues to press Moscow for his release',
+    content: `
+    Wall Street Journal reporter Evan Gershkovich has appeared in a Russian court to stand trial in a secret proceeding on charges of espionage amid US efforts to secure his release in a prisoner exchange.
+
+    Video footage published by state media showed Gershkovich standing in a glass cage at a district court in Ekaterinburg, a city in Russia’s defence-industrial heartland in the Ural Mountains, ahead of the hearing on Wednesday.
+
+    Gershkovich faces a possible sentence of up to 20 years in prison if he is found guilty of espionage.
+
+    Officials have not provided any evidence of Gershkovich’s alleged guilt, despite the Kremlin’s claim to have caught him “red-handed,” or given any information on the scope and duration of his trial.
+
+    US officials are negotiating with Russia for a potential prisoner swap involving Gershkovich and Paul Whelan, a US marine veteran jailed for 16 years on similar charges in 2020, in exchange for Russians held in the west.
+
+    The US embassy in Moscow on Wednesday said Gershkovich “has done nothing wrong and never should have been arrested. His case is not about evidence, procedural norms or the rule of law. It is about the Kremlin using American citizens to achieve its political objectives.
+
+    “Russia should stop using individuals like Evan Gershkovich or Paul Whelan as bargaining chips. They should both be released immediately.”
+
+    Russia’s President Vladimir Putin has said the Kremlin is open to an exchange involving Gershkovich through a “quiet, calm professional approach and dialogue between the secret services”.
+
+    Prosecutors claimed Gershkovich was in Ekaterinburg on an assignment for the CIA to obtain “secret information” about production and repair efforts at Uralvagonzavod, a large tank factory, when he was arrested in March 2023.
+
+    The WSJ vehemently denies the charges against Gershkovich, while the US has declared that he has been wrongfully detained.
+
+    Emma Tucker, the WSJ’s editor in chief, said the proceedings would “not be a trial as we understand it, with a presumption of innocence and a search for the truth”, in a letter to readers published on Wednesday.
+
+    “We already know the conclusion,” she added. “This bogus accusation of espionage will inevitably lead to a bogus conviction for an innocent man who would then face up to 20 years in prison for simply doing his job.”
+
+    Gershkovich is one of several journalists arrested in Russia after Putin promptly banned all dissent following his full-scale invasion of Ukraine in 2022.
+
+    They include Alsu Kurmasheva, a US-Russian dual national journalist at Radio Free Europe/Radio Liberty who was arrested while visiting family last year. She faces up to 10 years in prison on charges of not registering as a “foreign agent” and “disseminating false information” about Russia’s armed forces, which she denies.
+
+    Gershkovich’s probable conviction is likely to be a prelude to a potential prisoner swap that could involve other US citizens jailed in Russia.
+
+    The US has previously swapped high-profile Russian prisoners for Americans in Russian custody, including arms dealer Viktor Bout who was traded for US basketball star Brittney Griner following her sentencing on drug charges in 2022.
+
+    Putin strongly hinted in February that he was demanding in exchange the release of Vadim Krasikov, a hitman sentenced to life in prison in Germany in 2021.
+
+    Efforts to swap Krasikov for Gershkovich, Whelan and jailed opposition leader Alexei Navalny, however, collapsed when the dissident died in a remote Arctic penal colony months later.
+
+    Navalny’s supporters have accused Putin of murdering him to scupper the deal. Putin said in March he had agreed to an informal proposal to release Navalny on the condition that he never returned to Russia but denied any involvement in his “tragic” death.
+
+    People briefed on the talks say the Russian president is fixated on releasing Krasikov, who murdered a former Chechen militant in broad daylight in a Berlin park in 2019.
+
+    Russia has jailed several more Americans in recent months, including Gordon Black, a US soldier who was arrested on theft charges while visiting his girlfriend in the eastern city of Vladivostok in May and sentenced to nearly four years in prison last week.
+
+    Ksenia Khavana, a US-Russian dual national, went on trial last week in Ekaterinburg on treason charges for sending a pro-Ukrainian group $51.80 on the first day of Moscow’s invasion. Judge Andrei Mineyev, who is also overseeing Gershkovich’s case, ordered proceedings to be held in secret in that trial too.
+`,
   },
 ];
